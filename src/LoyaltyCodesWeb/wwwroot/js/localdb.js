9898e6c805db6aeaa765f9118e174d0b8bca18c9
Alexander Lea <alexander.lea@gmail.com>
2016-01-23 18:33:13 +0000

    promises and angular factory

--- src/LoyaltyCodesWeb/wwwroot/js/localdb.js
+++ src/LoyaltyCodesWeb/wwwroot/js/localdb.js
@@ -1,17 +1,6 @@
-﻿(function() {
-    var COMPAT_ENVS = [
-        ['Firefox', ">= 16.0"],
-        [
-            'Google Chrome',
-            ">= 24.0 (you may need to get Google Chrome Canary), NO Blob storage support"
-        ]
-    ];
-    var compat = $('#compat');
-    compat.empty();
-    compat.append('<ul id="compat-list"></ul>');
-    COMPAT_ENVS.forEach(function(val, idx, array) {
-        $('#compat-list').append('<li>' + val[0] + ': ' + val[1] + '</li>');
-    });
+﻿var app = angular.module('loyaltyCardApp', []);
+
+app.factory('indexedDb', function ($window, $q) {
 
     const DB_NAME = 'LoyaltyCodesDb';
     const DB_VERSION = 2; // Use a long long for this value (don't use a float)
@@ -20,28 +9,34 @@
     var db;
 
     function openDb() {
-        console.log("openDb ...");
-        var req = indexedDB.open(DB_NAME, DB_VERSION);
-        req.onsuccess = function(evt) {
-            // Better use "this" than "req" to get the result to avoid problems with
-            // garbage collection.
-            // db = req.result;
-            db = this.result;
-            console.log('openDb DONE');
-        };
-        req.onerror = function(evt) {
-            console.error('openDb:', evt.target.errorCode);
-        };
+        var promise = new Promise(function (resolve, reject) {
+            console.log("openDb ...");
+            var req = indexedDB.open(DB_NAME, DB_VERSION);
+            req.onsuccess = function (evt) {
+                // Better use "this" than "req" to get the result to avoid problems with
+                // garbage collection.
+                // db = req.result;
+                db = this.result;
+                console.log('openDb DONE');
+                resolve();
+            };
 
-        req.onupgradeneeded = function(evt) {
-            console.log("openDb.onupgradeneeded");
-            var store = evt.currentTarget.result.createObjectStore(
-                DB_STORE_NAME, { keyPath: 'Id', autoIncrement: true });
+            req.onerror = function (evt) {
+                console.error('openDb:', evt.target.errorCode);
+                reject('Error opening db:', evt.target.errorCode);
+            };
 
-            store.createIndex("Name", "Name", { unique: false });
-            store.createIndex("Description", "Description", { unique: false });
-            store.createIndex("Barcode", "Barcode", { unique: false });
-        };
+            req.onupgradeneeded = function (evt) {
+                console.log("openDb.onupgradeneeded");
+                var store = evt.currentTarget.result.createObjectStore(
+                    DB_STORE_NAME, { keyPath: 'Id', autoIncrement: true });
+
+                store.createIndex("Name", "Name", { unique: false });
+                store.createIndex("Description", "Description", { unique: false });
+                store.createIndex("Barcode", "Barcode", { unique: false });
+            };
+        });
+        return promise;
     }
 
     /**
@@ -53,96 +48,98 @@
         return tx.objectStore(store_name);
     }
 
-    function clearObjectStore(store_name) {
-        var store = getObjectStore(DB_STORE_NAME, 'readwrite');
-        var req = store.clear();
-        req.onsuccess = function(evt) {
-            displayActionSuccess("Store cleared");
-            displayPubList(store);
-        };
-        req.onerror = function(evt) {
-            console.error("clearObjectStore:", evt.target.errorCode);
-            displayActionFailure(this.error);
-        };
-    }
-
-    function getBlob(key, store, success_callback) {
-        var req = store.get(key);
-        req.onsuccess = function (evt) {
-            var value = evt.target.result;
-            if (value)
-                success_callback(value.blob);
-        };
-    }
-
     /**
    * @param {object} Card
    */
     function addCard(card) {
 
         var store = getObjectStore(DB_STORE_NAME, 'readwrite');
-        var req = store.add(card);
- 
-        req.onsuccess = function (evt) {
-            console.log("Insertion in DB successful");
-        };
-        req.onerror = function () {
-            console.error("Error Inserting into db", this.error);
-        };
+
+        var promise = new Promise(function(resolve, reject) {
+            var req = store.add(card);
+
+            req.onsuccess = function(evt) {
+                console.log("Insertion in DB successful");
+                resolve();
+            };
+            req.onerror = function() {
+                console.error("Error Inserting into db", this.error);
+                reject("Error Inserting into db", this.error);
+            };
+        });
+
+        return promise;
     }
 
     function getAllCards() {
         console.log("Get all cards");
         var store = getObjectStore(DB_STORE_NAME, 'readonly');
 
-        var req = store.openCursor();
+        var promise = new Promise(function (resolve, reject) {
+            var req = store.openCursor();
 
-        req.onsuccess = function (evt) {
-            var cardList = [];
-            var cursor = evt.target.result;
+            req.onsuccess = function (evt) {
+                var cardList = [];
+                var cursor = evt.target.result;
 
-            // If the cursor is pointing at something, ask for the data
-            if (cursor) {
-                console.log("getAllCards cursor:", cursor);
-                req = store.get(cursor.key);
-                req.onsuccess = function (evt) {
-                    var value = evt.target.result;
+                // If the cursor is pointing at something, ask for the data
+                if (cursor) {
+                    console.log("getAllCards cursor:", cursor);
+                    req = store.get(cursor.key);
+                    req.onsuccess = function (evt) {
+                        var value = evt.target.result;
 
-                    var card = {
-                        Id: value.Id,
-                        Name: value.Name,
-                        Description: value.Description,
-                        Barcode: value.Barcode
-                    }
+                        var card = {
+                            Id: value.Id,
+                            Name: value.Name,
+                            Description: value.Description,
+                            Barcode: value.Barcode
+                        }
 
-                    cardList.push(card);                   
-                };
+                        cardList.push(card);
+                    };
 
-                // Move on to the next object in store
-                cursor.continue();
-            } else {
-                console.log("No more entries");
+                    // Move on to the next object in store
+                    cursor.continue();
+                } else {
+                    console.log("No more entries");
+                    resolve(cardList);
+                }
+            };
+
+            req.onerror = function(e) {
+                console.error('openDb:', evt.target.errorCode);
+                reject('Error opening db:', evt.target.errorCode);
             }
-
-            return cardList;
-        };
+        });
+        return promise;
     }
 
     function getCard(id) {
         var store = getObjectStore(DB_STORE_NAME, 'readonly');
 
-        var req = store.get(id);
+        var promise = new Promise(function(resolve, reject) {
+            var req = store.get(id);
 
-        req.onerror = function (event) {
-            // Handle errors!
-            console.error("Error getting data for", id);
-        };
-        req.onsuccess = function (event) {
-            // Do something with the request.result!
-            return req.result;
-        };
+            req.onsuccess = function (event) {
+                // Do something with the request.result!
+                resolve(req.result);
+            };
+
+            req.onerror = function(event) {
+                // Handle errors!
+                console.error("Error getting data for", id);
+                reject("Error getting data for", id);
+            };
+
+        });
+
+        return promise;
     }
 
-
-    openDb();
-})();
+    return {
+        openDb: openDb,
+        getCard: getCard,
+        getAllCards: getAllCards,
+    };
+});
