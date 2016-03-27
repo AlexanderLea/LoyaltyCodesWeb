2759061133d5c022afbc1744207770becdcc7a05
Alexander Lea <alexander.lea@gmail.com>
2016-03-27 20:42:34 +0100

    pouchdb rather than indexeddb

--- src/LoyaltyCodesWeb/wwwroot/js/CardController.js
+++ src/LoyaltyCodesWeb/wwwroot/js/CardController.js
@@ -7,38 +7,24 @@
         function ($rootScope, $scope, $window, $location, indexedDbFactory) {
             $rootScope.title = 'Loyalty Cards - View Cards';
 
-            var dbOpen = false;
-
             $scope.cardList = [];
 
             function loadData() {
-                indexedDbFactory.openDb().then(function () {
-                    dbOpen = true;
-                    refreshCards();
+                refreshCards();
+            }
+
+            function refreshCards() {
+                indexedDbFactory.getAllCards().then(function (data) {
+                    $scope.cardList = data;
+                    $scope.$apply();
                 }, function (error) {
                     // log errors
                     console.log('error.', error);
                 });
             }
 
-            function refreshCards() {
-                if (dbOpen) {
-                    indexedDbFactory.getAllCards().then(function (data) {
-                        $scope.cardList = data;
-                        $scope.$apply();
-                    }, function (error) {
-                        // log errors
-                        console.log('error.', error);
-                    });
-                }
-                else
-                {
-                    loadData();
-                }
-            }
-
             function showCard(card) {
-                $location.path('/view/' + card.Id);
+                $location.path('/view/' + card._id);
             }
 
             $scope.showCard = showCard;
@@ -50,20 +36,13 @@
         '$rootScope', '$scope', '$window', 'indexedDbFactory',
         function ($rootScope, $scope, $window, indexedDbFactory) {
             $rootScope.title = 'Loyalty Cards - Create Card';
-            var dbOpen = false;
-            
-            indexedDbFactory.openDb().then(function () {
-                dbOpen = true;
-            }, function (error) {
-                // log errors
-                console.log('error.', error);
-            });
 
-            $scope.addCard = function () {               
+            $scope.addCard = function () {
                 var card = {
                     Name: $scope.Name,
                     Description: $scope.Description,
-                    Barcode: $scope.Barcode
+                    Barcode: $scope.Barcode,
+                    Colour: $scope.Colour
                 }
 
                 indexedDbFactory.addCard(card).then(function () {
@@ -78,32 +57,23 @@
     app.controller('viewCardController', ['$rootScope', '$scope', '$window', '$routeParams', 'indexedDbFactory',
         function ($rootScope, $scope, $window, $routeParams, indexedDbFactory) {
             $rootScope.title = 'Loyalty Cards - Card Details';
-            var isDbOpen = false;
 
             //$scope.message = id;
             $scope.card = null;
-            
+
             function loadCard() {
                 var id = $routeParams.id;
-                if (isDbOpen) {
-                    indexedDbFactory.getCard(id).then(function (card) {
-                        $scope.card = card;
-                        $scope.$apply();
-                    }, function (error) {
-                        // log errors
-                        $scope.error = 'There was an error loading the selected card';
-                        console.log('error.', error);
-                        $scope.$apply();
-                    });                    
-                } else {
-                    indexedDbFactory.openDb().then(function() {
-                        isDbOpen = true;
-                        loadCard();
-                    }, function (error) {
-                        // log errors
-                        console.log('error.', error);
-                    });
-                }            
+
+                indexedDbFactory.getCard(id).then(function (card) {
+                    $scope.card = card;
+                    $scope.$apply();
+                }, function (error) {
+                    // log errors
+                    $scope.error = 'There was an error loading the selected card';
+                    console.log('error.', error);
+                    $scope.$apply();
+                });
+
             }
 
             loadCard();
--- src/LoyaltyCodesWeb/wwwroot/js/indexedDbFactory.js
+++ src/LoyaltyCodesWeb/wwwroot/js/indexedDbFactory.js
@@ -3,69 +3,23 @@
 
 
     var app = angular.module('LoyaltyCardApp');
-    
+
     app.factory('indexedDbFactory', [indexedDbFactory]);
 
-
     function indexedDbFactory() {
-
-
-        const DB_NAME = 'LoyaltyCodesDb';
-        const DB_VERSION = 2; // Use a long long for this value (don't use a float)
-        const DB_STORE_NAME = 'Cards';
-
-        var db;
-
-        function openDb() {
-            var promise = new Promise(function (resolve, reject) {
-
-                var req = indexedDB.open(DB_NAME, DB_VERSION);
-
-                req.onsuccess = function (evt) {
-                    db = this.result;
-                    console.log('Db open');
-                    resolve();
-                };
-
-                req.onerror = function (evt) {
-                    console.error('openDb:', evt.target.errorCode);
-                    reject('Error opening db:', evt.target.errorCode);
-                };
-
-                req.onupgradeneeded = function (evt) {
-                    console.log("openDb.onupgradeneeded");
-                    var store = evt.currentTarget.result.createObjectStore(
-                        DB_STORE_NAME, { keyPath: 'Id', autoIncrement: true });
-
-                    store.createIndex("Name", "Name", { unique: false });
-                    store.createIndex("Description", "Description", { unique: false });
-                    store.createIndex("Barcode", "Barcode", { unique: false });
-                };
-            });
-
-            return promise;
-        }
-
-        function getObjectStore(storeName, mode) {
-            var tx = db.transaction(storeName, mode);
-            return tx.objectStore(storeName);
-        }
+        var db = new PouchDB('offlineLoyaltyCards');
 
         function addCard(card) {
 
-            var store = getObjectStore(DB_STORE_NAME, 'readwrite');
-
             var promise = new Promise(function (resolve, reject) {
-                var req = store.add(card);
 
-                req.onsuccess = function (evt) {
-                    console.log("Insertion in DB successful");
+                db.post(card).then(function (response) {
+                    // handle response
                     resolve();
-                };
-                req.onerror = function () {
-                    console.error("Error Inserting into db", this.error);
+                }).catch(function (err) {
+                    console.log(err);
                     reject("Error Inserting into db", this.error);
-                };
+                });
             });
 
             return promise;
@@ -73,78 +27,47 @@
 
         function getAllCards() {
             console.log('Get all cards');
-            var store = getObjectStore(DB_STORE_NAME, 'readonly');
-
             var promise = new Promise(function (resolve, reject) {
-                var req = store.openCursor();
-                var cardList = [];
 
-                req.onsuccess = function (evt) {
-                    var cursor = evt.target.result;
 
-                    // If the cursor is pointing at something, ask for the data
-                    if (cursor) {
-                        req = store.get(cursor.key);
-                        req.onsuccess = function (evt) {
-                            var value = evt.target.result;
+                db.allDocs({
+                    include_docs: true,
+                    attachments: true
+                }).then(function (result) {
+                    // handle result
+                    var cards = [];
 
-                            var card = {
-                                Id: value.Id,
-                                Name: value.Name,
-                                Description: value.Description,
-                                Barcode: value.Barcode
-                            }
+                    result.rows.forEach(function (e) {
+                        cards.push(e.doc);
+                    });
 
-                            cardList.push(card);
-                        };
-
-                        // Move on to the next object in store
-                        cursor.continue();
-                    } else {
-                        console.log("No more entries. cardList:", cardList);
-                        resolve(cardList);
-                    }
-                };
-
-                req.onerror = function (e) {
-                    console.error('openDb:', e.target.errorCode);
-                    reject('Error opening db:', e.target.errorCode);
-                }
+                    resolve(cards);
+                }).catch(function (err) {
+                    console.log(err);
+                    reject('Error opening db:', e.target.errorCode)
+                });
             });
             return promise;
         }
 
         function getCard(id) {
-            var store = getObjectStore(DB_STORE_NAME, 'readonly');
+            //var store = getObjectStore(DB_STORE_NAME, 'readonly');
 
             var promise = new Promise(function (resolve, reject) {
-                var key = parseInt(id);
-                var req = store.get(key);
 
-                req.onsuccess = function (event) {
-                    // Do something with the request.result!
-                    var value = event.target.result;
-
-                    if (value)
-                        resolve(value);
-                    else {
-                        console.log('no values returned for id:', id);
-                    }
-                };
-
-                req.onerror = function (event) {
-                    // Handle errors!
-                    console.error("Error getting data for", id);
+                db.get(id).then(function (doc) {
+                    // handle doc
+                    resolve(doc);
+                }).catch(function (err) {
+                    console.log(err);
                     reject("Error getting data for", id);
-                };
-
+                });
             });
 
             return promise;
         }
 
         return {
-            openDb: openDb,
             getCard: getCard,
             getAllCards: getAllCards,
             addCard: addCard
